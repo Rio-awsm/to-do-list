9b903ca1d700d2109252610bbb647fad9acfac01
Supriyo Saha <113161406+Rio-awsm@users.noreply.github.com>
2022-12-27 11:38:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const date = require(__dirname +"/date.js");
+
+const app = express();
+
+let items =["Buy food","Cook food","Eat food"];
+let workItems =[];
+
+app.set("view engine",'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    
+   let day = date.getDate();
+   
+    res.render("list", {listTitle: day,newListItems: items });
+});
+
+app.post("/", function(req, res){
+    let item = req.body.newItem;
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+       
+});
+
+app.get("/work", function(req,res){
+   res.render("list",{listTitle: "Work List",newListItems: workItems});
+});
+
+app.post("/work", function(req,res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+
+app.get("/about", function(req,res){
+   res.render("about");
+});
+
+app.listen(3000, function(){
+    console.log("server started on port 3000");
+});
---
+++ date.js
@@ -0,0 +1,22 @@
+module.exports.getDate = getDate;
+
+function getDate(){
+let today = new Date();
+let options = {
+    weekday: "long",
+    day:"numeric",
+    month:"long"
+};
+return today.toLocaleDateString("en-US", options); 
+
+}
+
+module.exports.getDay = getDay;
+function getDay(){
+    let today = new Date();
+    let options = {
+        weekday: "long"
+    };
+     return today.toLocaleDateString("en-US", options); 
+    }
+
